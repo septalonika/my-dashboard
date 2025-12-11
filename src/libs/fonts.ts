4ec6febf8dfbc7dce09715042e7fb2a26ac319f2
septalonika <andikabeatbox@gmail.com>
2025-12-11 14:15:39 +0700

    add fonts: - Inter - Roboto

---
+++ src/libs/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700", "800"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700", "800"],
+});
